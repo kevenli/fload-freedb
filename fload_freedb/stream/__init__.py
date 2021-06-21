d49ce7c1e7616f7a3e8c74b7ce8f2bd2fcac3650
Keven Li <pbleester@gmail.com>
2021-06-21 10:36:58 +0800

    freedb filter

--- fload_freedb/stream/__init__.py
+++ fload_freedb/stream/__init__.py
@@ -1,5 +1,7 @@
 from .tofreedb import ToFreedb
 from .fromfreedb import FreedbSource
+from .freedbfilter import FreedbDocNotExistFilter
 
 to_freedb = ToFreedb
 freedb = FreedbSource
+freedb_doc_not_exist = FreedbDocNotExistFilter
---
+++ fload_freedb/stream/freedbfilter.py
@@ -0,0 +1,37 @@
+from fload import Pipeline
+
+from fload_freedb.freedb import FreedbClient, FreedbCollection, DocumentDotExist
+
+class FreedbDocNotExistFilter(Pipeline):
+    freedb_url = None
+    token = None
+    db = None
+    collection = None
+    col:FreedbCollection = None
+
+    def add_arguments(self, parser):
+        parser.add_argument('--freedb-url', default='http://localhost:8000/')
+        parser.add_argument('--token')
+        parser.add_argument('--db')
+        parser.add_argument('--collection')
+
+    def init(self, ops):
+        self.freedb_url = ops.freedb_url
+        self.token = ops.token
+        self.db = ops.db
+        self.collection = ops.collection
+
+        client = FreedbClient(self.freedb_url, token=self.token)
+        db = client.database(self.db)
+        self.col = db.collection(self.collection)
+
+    def process(self, item):
+        id = item.get('id')
+        if id is None:
+            return item
+
+        try:
+            doc = self.col.doc(id)
+            return None
+        except DocumentDotExist:
+            return item
