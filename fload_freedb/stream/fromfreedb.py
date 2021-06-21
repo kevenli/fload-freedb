0144a8eedb4d7abc3c0b9641f7b6c5a1ec0301e1
Keven Li <pbleester@gmail.com>
2021-06-21 11:17:11 +0800

    freedb source --query parameter

--- fload_freedb/stream/fromfreedb.py
+++ fload_freedb/stream/fromfreedb.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+import json
 
 from fload import Source
 from fload_freedb.freedb import FreedbClient, FreedbCollection
@@ -9,10 +10,15 @@ class FreedbSource(Source):
     token = None
     db = None
     collection = None
-    col:FreedbCollection = None
+    col: FreedbCollection = None
+    query: str = None
 
     def start(self):
-        for item in self.col.iter():
+        params = {}
+        if self.query:
+            params['query'] = json.loads(self.query)
+            
+        for item in self.col.iter(**params):
             yield item
 
     def add_arguments(self, parser:ArgumentParser):
@@ -20,12 +26,14 @@ class FreedbSource(Source):
         parser.add_argument('--token')
         parser.add_argument('--db')
         parser.add_argument('--collection')
+        parser.add_argument('--query')
     
     def init(self, ops):
         self.freedb_url = ops.freedb_url
         self.token = ops.token
         self.db = ops.db
         self.collection = ops.collection
+        self.query = ops.query
 
         client = FreedbClient(self.freedb_url, token=self.token)
         db = client.database(self.db)
